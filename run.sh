4893e5c03c4de0039efa5b79dc7ee7f52fd38462
tanjiti <danqingdani@gmail.com>
2019-03-08 14:53:44 +0800

    2019-03-08 00:03:00

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-ts=$(date "+%Y-%m-%d %H:%m:%S")
-
-#python update_daily.py
-git add .
-cmd="git commit -m '$ts'"
-eval $cmd
-git commit -m ''
-git push origin master
--- update_daily.py
+++ update_daily.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+import os
 import sys
 
 reload(sys)
@@ -8,6 +8,7 @@ sys.setdefaultencoding('utf8')
 from secwiki_today import scraw as secwiki_scraw
 from xuanwu_today import scraw as xuanwu_scraw
 from mills import SQLiteOper
+from mills import get_special_date
 import visual
 
 
@@ -22,6 +23,17 @@ def scraw():
 
     xuanwu_scraw(so, proxy=proxy, delta=2)
 
+def update_github():
+    """
+
+    :return:
+    """
+    ts = get_special_date(format="%Y-%m-%d %H:%m:%S")
+    cmd = "git add . && git commit -m '%s' && git push origin master" %(ts)
+
+    ret = os.system(cmd)
+    if ret != 0:
+        print "%s failed" %cmd
 
 if __name__ == "__main__":
     """
@@ -29,3 +41,5 @@ if __name__ == "__main__":
     scraw()
 
     visual.draw_readme()
+
+    update_github()
