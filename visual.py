4af3d72314bdf9e737df1a83a4066d81154a6758
tanjiti <danqingdani@gmail.com>
2019-04-04 08:30:29 +0800

    2019-04-04 00:04:00

--- visual.py
+++ visual.py
@@ -277,6 +277,7 @@ def draw_readme(fpath=None):
     so = SQLiteOper("data/scrap.db")
     year = get_special_date(delta=0, format="%Y%m")
     # update
+    year='201903'
     main_pie(year)
 
     # update weixin,github
