cd0ca88c54937f2ea99f126d6ec40bb5e2f7074a
tanjiti <danqingdani@gmail.com>
2019-08-03 23:14:21 +0800

    2019-08-03 00:08:00

--- xuanwu_today.py
+++ xuanwu_today.py
@@ -170,6 +170,6 @@ if __name__ == "__main__":
         "socks:": "socks://127.0.0.1:8420",
 
     }
-    proxy = None
+    #proxy = None
     so = SQLiteOper("data/scrap.db")
     scraw(so, proxy=proxy)
