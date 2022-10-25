e8c2a9509a8276018a61a5d7fdb90dd25bc2158f
tanjiti <danqingdani@gmail.com>
2022-10-25 16:05:55 +0800

    2022-10-25 00:10:00

--- update_daily.py
+++ update_daily.py
@@ -51,11 +51,11 @@ if __name__ == "__main__":
     """
     proxy = {
         # "socks:": "socks://127.0.0.1:1080",
-        "http": "http://127.0.0.1:1081",
-        "https": "http://127.0.0.1:1081"
+        "http": "socks5://127.0.0.1:1081",
+        "https": "socks5://127.0.0.1:1081"
 
     }
-    proxy = None
+    #proxy = None
 
     scraw(proxy=proxy)
 
